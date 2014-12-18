1e1e1cb2165583b454e628da9b71bd1af59e657e
Jacob Niehus <jacob.niehus@gmail.com>
2014-12-17 18:57:34 -0700

    Add fzf.zsh

---
+++ fzf.zsh
@@ -0,0 +1,125 @@
+# Setup fzf function
+# ------------------
+unalias fzf 2> /dev/null
+fzf() {
+  $FZF_RUBY_EXEC --disable-gems $HOME/.fzf/fzf "$@"
+}
+export -f fzf > /dev/null
+[[ -z $FZF_DEFAULT_OPTS ]] && export FZF_DEFAULT_OPTS="+c -m"
+
+# Auto-completion
+# ---------------
+[[ $- =~ i ]] && source $HOME/.fzf/fzf-completion.zsh
+
+# Key bindings
+# ------------
+# CTRL-T - Paste the selected file path(s) into the command line
+__fsel() {
+  set -o nonomatch
+  $FZF_DEFAULT_COMMAND 2> /dev/null | fzf -m | while read item; do
+    printf '%q ' "$item"
+  done
+  echo
+}
+
+if [[ $- =~ i ]]; then
+
+if [ -n "$TMUX_PANE" -a ${FZF_TMUX:-1} -ne 0 -a ${LINES:-40} -gt 15 ]; then
+  fzf-file-widget() {
+    local height
+    height=${FZF_TMUX_HEIGHT:-40%}
+    if [[ $height =~ %$ ]]; then
+      height="-p ${height%\%}"
+    else
+      height="-l $height"
+    fi
+    tmux split-window $height "zsh -c 'cd $PWD; source ~/.fzf.zsh; \
+      tmux send-keys -t $TMUX_PANE \"\$(__fsel)\"'"
+  }
+else
+  fzf-file-widget() {
+    LBUFFER="${LBUFFER}$(__fsel)"
+    zle redisplay
+  }
+fi
+zle     -N   fzf-file-widget
+# bindkey '^T' fzf-file-widget
+
+# ALT-C - cd into the selected directory
+fzf-cd-widget() {
+  cd "${$(set -o nonomatch; command find * -path '*/\.*' -prune \
+          -o -type d -print 2> /dev/null | fzf):-.}"
+  zle reset-prompt
+}
+zle     -N    fzf-cd-widget
+bindkey 'ã' fzf-cd-widget
+
+# CTRL-R - Paste the selected command from history into the command line
+fzf-history-widget() {
+  newbuffer=$(fc -l 1         \
+    | LC_ALL='C' sort -k 2 -r \
+    | LC_ALL='C' uniq -f 1    \
+    | LC_ALL='C' sort -n      \
+    | sed 's/ ###.*$//'       \
+    | fzf +s +m -n..,1,2..    \
+    | sed "s/ *[0-9*]* *//")
+  lines=(${(s:\n:)newbuffer})
+  if [[ ${#lines} -eq 1 ]]; then
+    LBUFFER=$newbuffer
+  else
+    for line in ${lines[1,-2]}; do
+      LBUFFER=${LBUFFER}${line}
+      zle vi-open-line-below
+    done
+    LBUFFER=${LBUFFER}${lines[-1]}
+  fi
+  zle redisplay
+}
+zle     -N   fzf-history-widget
+# bindkey '^R' fzf-history-widget
+
+# ALT-R - Paste the selected command from directory history into the command line
+fzf-dir-history-widget() {
+  newbuffer=$(dirhist $PWD | fzf +s +m)
+  lines=(${(s:\n:)newbuffer})
+  if [[ ${#lines} -eq 1 ]]; then
+    LBUFFER=$newbuffer
+  else
+    for line in ${lines[1,-2]}; do
+      LBUFFER=${LBUFFER}${line}
+      zle vi-open-line-below
+    done
+    LBUFFER=${LBUFFER}${lines[-1]}
+  fi
+  zle redisplay
+}
+zle     -N           fzf-dir-history-widget
+bindkey -M viins 'ò' fzf-dir-history-widget  # <M-r>
+
+# ALT-D - cd into recent directory
+fzf-recent-directory-widget() {
+  local dir="$(sed -e "s/^\$'\(.*\)'$/\1/" -e '1!G;h;$!d' \
+    $HOME/.chpwd-recent-dirs | fzf +s)"
+  if [[ -n $dir ]]; then
+    # Escape special characters
+    for char in '*' '(' ')' '|' '<' '>' '[' ']' '?' ' '; do
+      dir=${dir//$char/\\$char};
+    done
+    LBUFFER=$LBUFFER$dir
+  fi
+  zle reset-prompt
+}
+zle -N fzf-recent-directory-widget
+bindkey ä fzf-recent-directory-widget
+
+fzf-combined-widget() {
+  if [[ -z $BUFFER ]]; then
+    zle fzf-history-widget
+  else
+    zle fzf-file-widget
+  fi
+}
+zle     -N   fzf-combined-widget
+bindkey '^R' fzf-combined-widget
+
+fi
